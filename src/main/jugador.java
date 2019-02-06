e92a912ae6d62700d9199190488b471339005372
masterasel96 <jjtm1996@gmail.com>
2019-02-06 14:24:30 +0100

    aa

--- src/main/jugador.java
+++ src/main/jugador.java
@@ -23,29 +23,7 @@ public class jugador {
         
     }
 
-    public String getNombre() {
-        return nombre;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public int getVotos() {
-        return votos;
-    }
-
-    public void setVotos(int votos) {
-        this.votos = votos;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
+    
     
    
 }
