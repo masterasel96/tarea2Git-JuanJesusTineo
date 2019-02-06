ef0b6b6568ee3c3c1ed6648a1f41f460eb5e5ec4
masterasel96 <jjtm1996@gmail.com>
2019-02-06 14:22:54 +0100

    Añade getters y setters

--- src/main/jugador.java
+++ src/main/jugador.java
@@ -22,4 +22,30 @@ public class jugador {
         this.id=id;
         
     }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getVotos() {
+        return votos;
+    }
+
+    public void setVotos(int votos) {
+        this.votos = votos;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+    
+   
 }
